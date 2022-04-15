8f2ea28b79c5422c106412581b98bc1aa19ed9c5
Jason.Chang <chaoyue.chang@gmail.com>
2022-04-15 16:57:35 +0800

    支持 php 8.1

--- travis-build.sh
+++ travis-build.sh
@@ -20,7 +20,7 @@ cat .env
 docker-compose build ${BUILD_SERVICE}
 docker images
 
-#### Generate the Laradock Documentation site using Hugo
+#### Generate the Cantor Documentation site using Hugo
 if [ -n "${HUGO_VERSION}" ]; then
     HUGO_PACKAGE=hugo_${HUGO_VERSION}_Linux-64bit
     HUGO_BIN=hugo_${HUGO_VERSION}_linux_amd64
