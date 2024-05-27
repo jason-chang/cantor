599767a045ef51ee6ed5fccc489bd8491e802c48
jason_chang <chaoyue.chang@gmaill.com>
2024-05-28 01:41:42 +0800

    fix: fix mirror options

--- node-worker/mirror.sh
+++ node-worker/mirror.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
-package_managers=(
-  "npm"
-  "yarn"
-  "pnpm"
-)
-
 # Set the registry for all package managers
-for package in "${package_managers[@]}"; do
+set_registry() {
+  package=$1
   echo "Setting registry for $package"
-  $package config set registry https://registry.npm.taobao.org
-  $package config set disturl https://npmmirror.com/mirrors
-  $package config set electron_mirror https://npmmirror.com/mirrors/electron/ 
-  $package config set ELECTRON_MIRROR https://npmmirror.com/mirrors/electron/
-  $package config set chromedriver_cdnurl https://npmmirror.com/mirrors/chromedriver
-  $package config set operadriver_cdnurl https://npmmirror.com/mirrors/operadriver
-  $package config set phantomjs_cdnurl https://npmmirror.com/mirrors/phantomjs
-  $package config set selenium_cdnurl https://npmmirror.com/mirrors/selenium
-  $package config set node_inspector_cdnurl https://npmmirror.com/mirrors/node-inspector
+  $package config set registry https://registry.npmmirror.com
+}
+
+echo "npm yarn pnpm" | while read package; do
+  set_registry $package
 done
